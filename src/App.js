0f0215ad1224576cc7272db490343f1ab18dda31
Gulshan <honeykulta@gmail.com>
2018-12-18 17:48:40 +0200

    Testing Firebase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { database } from './firebase';
+import _ from 'lodash';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'',
+      address:'',
+      kohdet:{}
+      }
+      this.onValueChange = this.onValueChange.bind(this);
+      this.onSaveSubmit = this.onSaveSubmit.bind(this);
+      this.renderKohde = this.renderKohde.bind(this)
+  }
+
+  componentDidMount(){
+    database.on('value', snapshot => {
+      this.setState({
+        kohdet: snapshot.val()
+      });
+    })
+
+  }
+
+  onValueChange(e){
+    this.setState({
+      [e.target.name]:e.target.value
+    });
+  }
+
+  onSaveSubmit(e){
+    e.preventDefault();
+    console.log("submkit press");
+    const kohde = {
+      name: this.state.name,
+      address: this.state.address
+    }
+    database.push(kohde);
+    this.setState({
+      name:'',
+      address:''
+    })
+
+  }
+  renderKohde(){
+    return _.map(this.state.kohdet,(kohde, key) => {
+      return (
+        <div key="key">
+          <h3>{kohde.name}</h3>
+          <p>{kohde.address}</p>
+        </div>
+      )
+    });
+  }
+  render() { 
+    return ( 
+      <div>
+
+        <form onSubmit={this.onSaveSubmit}>
+        Name:<br />
+        <input type="text"
+        onChange={this.onValueChange}
+        value={this.state.name}
+        name="name" /><br />
+        Address:<br />
+        <input type="text" 
+        onChange={this.onValueChange}
+        value={this.state.address}
+        name="address" />
+         <input type="submit" value="Submit"></input>
+        </form>
+        {this.renderKohde()}
       </div>
-    );
+     );
   }
 }
-
+ 
 export default App;
