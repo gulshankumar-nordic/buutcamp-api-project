b317d870d38bd38b733f3610e91d0f19fc33c45d
Gulshan <honeykulta@gmail.com>
2018-12-31 03:06:33 +0200

    Added action and rendering

--- src/App.js
+++ src/App.js
@@ -1,111 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
 import './style/bootstrap.min.css'
-import { Container, Row, Col,  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
-
-  import 'ol/ol.css';
-
+import { Container, Row, Col } from 'reactstrap';
+import 'ol/ol.css';
 import _ from 'lodash';
-import { database } from './firebase';
-import L from 'leaflet';
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
-
+import { connect } from 'react-redux';
+import { getLocations, getKohdet, saveKohde, deleteKohde } from './actions/kohdeAction';
+//import 'leaflet/dist/leaflet.css'
+import {Link} from 'react-router-dom';
+import LocationCard from './LocationCard';
+import { getUser } from './actions/userAction'
+import Main from './Main';
 const provider = new OpenStreetMapProvider();
+
 class App extends Component {
 
+  state = {
+    markers: []
+  };
   constructor(props) {
     super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
+   
     this.state = {
       collapsed: true,
-      data: [],
       name:'',
       address:'',
-      kohdet:[],
-      zoom: 1,
       zoomOffset: 1,
       hasLocation: false,
       setLongitude:'',
-      setLatitude:'',
-      
+      setLatitude:'',    
+      test:[] ,
+      markers: {},
+      allMarkers: [],
+      lat: 65.021,
+      lng: 25.469,
+      zoom: 13,
     };
 
     this.onNameChange = this.onNameChange.bind(this);
     this.onAddressChange = this.onAddressChange.bind(this);
     this.onValueSubmit = this.onValueSubmit.bind(this);
-    this.searchFormSubmit = this.searchFormSubmit.bind(this);
   }
 
 
-  componentDidMount(){
-    
-      /* const map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        })
-      ],
-      view: new View({
-        zoom:8,
-        center: [49.8419, 24.0315],
-      })
-    })  */
+  /* componentDidMount(){
+    this.props.getKohdet();
+    this.props.getUser()
+    this.setState({kohdet: this.renderKohdet()
+    })
+  } */
 
-    database.on('value', snapshot => {
-      this.setState({ 
-        kohdet: snapshot.val(),
-       
-      });
-     
-    });
-
-    this.setState({kohdet: this.renderKohdet()})
-
-    let locations = [
-      ["LOCATION_1",65.0121, 25.4651],
-      ["LOCATION_4",62.1763, 25.3532],
-  
-      ];
-      console.log(this.state)
-          let marker=null;
-          let map = L.map('map').setView([65.0121, 25.4651], 8);
-          let mapLink = 
-              '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-          L.tileLayer(
-              'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-              attribution: '&copy; ' + mapLink + ' Contributors',
-              maxZoom: 18,
-              }).addTo(map);
-  
-      for (let i = 0; i < locations.length; i++) {
-        marker = new L.marker([locations[i][1],locations[i][2]])
-          //.bindPopup(locations[i][0])
-          .addTo(map);
-      }
-
-  }
-
-  
 
   renderKohdet(){
-    return _.map(this.state.kohdet, (value, key) => {
+    return _.map(this.props.kohdet, (value, key) => {
       return (
-        <div key={key}>
-          <h1>{value.name}</h1>
-          <p>{value.address}</p>
-        </div>
+       <LocationCard key={key} >
+        <Link to={`/${key}`}>
+          <h3>{value.name}</h3>
+        </Link>
+          <p>{value.address} - {value.setLongitude} - {value.setLatitude }</p>
+          {value.uid === this.props.user.uid && (
+            <div >
+            <button className="btn btn-danger" style={{width:'45%'}} onClick={()=>this.props.deleteKohde(key)}>delete</button>
+            <button className="btn btn-outline-info" style={{marginLeft:'10%', width:'45%'}}>
+            <Link to={`/${key}/update`}>Update</Link>
+            </button>
+           
+            </div>
+          )}
+        </LocationCard>
+      
       );
     });
   }
@@ -125,13 +91,8 @@ class App extends Component {
           setLatitude: res[0].y
         })
        }
-       
-      
      })
     } 
-     
-    //console.log('this is result - ', result);
-
   }
 
   onValueSubmit(e){
@@ -140,113 +101,85 @@ class App extends Component {
       name: this.state.name,
       address: this.state.address,
       setLatitude:this.state.setLatitude,
-      setLongitude: this.state.setLongitude
+      setLongitude: this.state.setLongitude,
+      uid: this.props.user.uid
     }
 
-    database.push(kohde);
+    //database.push(kohde);
+    this.props.saveKohde(kohde);
     this.setState({
       name:'',
-      address:''
+      address:'',
+      uid: this.props.user.uid
     })
-
-  }
-
-  searchFormSubmit(e){
-    e.preventDefault();
-    console.log("search button");
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
   }
   render() {
+    if(this.props.user === null ){
+      return (
+        <Main/>
+      )
+      
+    } else  {
     return (
       <div className="main-page" >
-        <Container>
-          <Row>
-            <Col xs="12">
-              <Navbar  light expand="md">
-                <NavbarBrand href="/">reactstrap</NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                  <Nav className="ml-auto" navbar>
-                    <NavItem>
-                      <NavLink href="/components/">Components</NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink href="#">GitHub</NavLink>
-                    </NavItem>
-                    <UncontrolledDropdown nav inNavbar>
-                      <DropdownToggle nav caret>
-                        Options
-                      </DropdownToggle>
-                      <DropdownMenu right>
-                        <DropdownItem>
-                          Option 1
-                        </DropdownItem>
-                        <DropdownItem>
-                          Option 2
-                        </DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem>
-                          Reset
-                        </DropdownItem>
-                      </DropdownMenu>
-                    </UncontrolledDropdown>
-                  </Nav>
-                </Collapse>
-              </Navbar>
-            </Col>
-          </Row>
-        </Container>
-        <Container>
+ 
+        <Container className="main-page">
          <Row>
-          <Col xs="6">
-            <div>
-           
+          <Col xs="5">
+            <div >
+          
               <hr/>
               <form onSubmit={this.onValueSubmit}>
+              <div className="form-group">
+                    <label>Name</label>
                 <input 
+                className="form-control"
                 type="text" 
                 name="name"
                 value={this.state.name}
                 onChange={this.onNameChange}
                 />
+                </div>
+                <div className="form-group">
+                    <label>Address</label>
                 <input 
+                className="form-control"
                 type="text"
                 name="address"
                 value={this.state.address}
                 onChange={this.onAddressChange}
                 />
+                </div>
                 <input type="hidden" name="longtitude" value={this.state.setLongitude.substring(0, 7)}/>
                <input type="hidden" name="latitude" value={this.state.setLatitude.substring(0, 7)}/>
-               
-               <input type="submit" value="Search"/>
-               
+               <input className="form-control" type="submit" value="Save"/>
               </form>
               <br/>
               <p>{this.state.setLongitude.substring(0, 7)} - Longitude</p>
               <p>{this.state.setLatitude.substring(0, 7)} - Latitude</p>
-              <hr />
-              {this.renderKohdet()}
+            
             </div>
           </Col>
-          <Col xs="6">
+          <Col xs="7">
             <div>
-              <div id='map' style={{width:600, height:400}}></div>
+            {this.renderKohdet()}
             </div>
           </Col>
         </Row>
-      
+        
         </Container>
       </div>
-
-       
-
     );
-  }
+    }
+  };
 }
 
-export default App;
+const mapStateToProps = (state,ownProps) => {
+  return {
+    kohdet: state.kohdet,
+    user: state.user
+  }
+};
+
+
+export default connect(mapStateToProps,{getKohdet, saveKohde, deleteKohde, getLocations, getUser})(App);
