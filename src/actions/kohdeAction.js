9123923d297e8f71c58c70fb6c73f9918cb15d48
Gulshan <honeykulta@gmail.com>
2018-12-31 03:12:29 +0200

    Modify getKohdet function

--- src/actions/kohdeAction.js
+++ src/actions/kohdeAction.js
@@ -1,17 +1,55 @@
-import { GET_KOHDET } from '../actions/actionTypes';
+import { GET_KOHDET, KOHDIEN_STATUS, GET_LOCATIONS } from '../actions/actionTypes';
 import { database } from '../firebase';
 
 export function getKohdet(){
     return dispatch => {
+        dispatch({
+            type: KOHDIEN_STATUS,
+            payload: true
+        });
         database.on('value', snapshot => {
             dispatch({
                 type: GET_KOHDET,
                 payload: snapshot.val()
             });
+            dispatch({
+                type: KOHDIEN_STATUS,
+                payload: false
+            });
+        }, 
+        () => {
+            dispatch({
+                type: KOHDIEN_STATUS,
+                payload: -1
+            });
+        });
+    };
+}
+
+export function getLocations(){
+    return dispatch => {
+        database.on('value', snapshot => {
+            dispatch({
+                type: GET_LOCATIONS,
+                payload: snapshot.val()
+            });
+          
         });
     };
 }
 
 export function saveKohde(kohde){
     return dispatch => database.push(kohde)
+}
+
+export function updateKohde(id, kohde){
+    return dispatch => database.child(id).update(kohde)
+}
+
+export function deleteKohde(id){
+    return dispatch => database.child(id).remove()
+}
+
+export function saveReview(kohdeId, review){
+    return dispatch => database.child(kohdeId).child('review').push(review)
 }
