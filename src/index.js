109e1185df546261fff8ba61f3cca70b030c358c
Gulshan <honeykulta@gmail.com>
2018-12-31 03:11:54 +0200

    Added routing, browserrouter and navigation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,44 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import Loading from './Loading';
+import Navigation from './Navigation';
+import Main from './Main'
+import Login from './Login';
+import Authentication from './Authentication';
+import LocationDetail from './LocationDetail';
+import LocationUpdate from './LocationUpdate';
+import Footer from './Footer';
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+<Provider store={store}>
+  <BrowserRouter>
+    <Loading>
+      <Navigation />
+      <Switch>
+      <Route path="/home" exact={true} component={Main}/>
+        <Route path="/login" exact={true} component={Login}/>
+        <Redirect from="/logout" to="/login" />
+        <Route path="/" exact={true} component={App}/>
+        <Authentication>      
+          <Route path="/" exact={true} component={App}/>
+          <Route path="/:id" component={LocationDetail} exact={true} />
+          <Route path="/:id/update" component={LocationUpdate} exact={true} />
+        </Authentication>
+      </Switch>
+      <Footer/>
+  
+  </Loading>
+  </BrowserRouter>
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
