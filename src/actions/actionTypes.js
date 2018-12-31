f07c22601da1119432e9af87b26e3d7620432250
Gulshan <honeykulta@gmail.com>
2018-12-31 03:10:48 +0200

    Added action types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,5 @@
 export const GET_KOHDET = 'GET_KOHDET';
+export const GET_LOCATIONS = 'GET_LOCATIONS';
+export const GET_USER = 'GET_USER';
+export const USER_STATUS = 'USER_STATUS';
+export const KOHDIEN_STATUS = 'KOHDIEN_STATUS';
