40e8029ced9e2d2b72999d923d7914844e523c63
Gulshan <honeykulta@gmail.com>
2018-12-31 03:08:53 +0200

    Added loading feature

---
+++ src/Loading.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {withRouter } from 'react-router-dom';
+import {getUser} from './actions/userAction';
+import { getKohdet } from './actions/kohdeAction';
+
+class Loading extends Component {
+    state = {  }
+
+    componentWillMount(){
+        const { userLoading, kohdeLoading } = this.props;
+        if (userLoading === undefined) {
+            this.props.getUser();
+        }
+        if( kohdeLoading === undefined ) {
+            this.props.getKohdet();
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.kohdeLoading === -1 && nextProps.user !== null ) {
+            this.props.getKohdet()
+        }
+    }
+    render() { 
+        const  { userLoading, kohdeLoading, children } = this.props;
+        if ((!userLoading && !kohdeLoading) || this.props.user === null ) {
+            return <div>{children}</div>
+        }
+        else {
+            return <div><h3 style={{color:'green'}}>Loaidng...</h3></div>
+        }
+      
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        userLoading: state.loading.user,
+        kohdeLoading: state.loading.kohdet,
+        user: state.user
+    }
+}
+ 
+export default withRouter(connect(mapStateToProps, {getKohdet, getUser})(Loading));
